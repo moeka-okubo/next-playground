b67ea050a9dadbd4343caa42742888801a93ffd2
moeka-okubo <moeka.okubo@arsaga.jp>
2024-07-07 14:27:12 +0900

    CSS Modulesの書き方修正

---
+++ next-playground-app/src/components/ComponentTestModule.tsx
@@ -0,0 +1,24 @@
+import { FC, memo } from "react";
+import style from "../styles/component.module.css";
+
+type Props = {
+  title?: string;
+  className?: string;
+};
+
+const ComponentTestModule: FC<Props> = ({
+  title = "デフォルトタイトル",
+  className,
+}) => {
+  return (
+    <>
+      <h2>↓は `container`というクラス名をつけている</h2>
+      <div className={`${style.component} ${className}`}>
+        <h2>{title}</h2>
+        コンポーネントのテスト
+      </div>
+    </>
+  );
+};
+
+export default memo(ComponentTestModule);
